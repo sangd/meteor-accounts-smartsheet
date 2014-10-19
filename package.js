62971727407bdd58233346b96a3131002a1d89c9
Sang Do <sdolight@gmail.com>
2014-10-19 13:08:24 -0700

    Add meteor-smartsheet version

--- package.js
+++ package.js
@@ -6,12 +6,13 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['underscore', 'random']);
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('sangd:meteor-smartsheet', ['client', 'server']);
+  api.use('sangd:meteor-smartsheet@1.0.0', ['client', 'server']);
 
   api.add_files('smartsheet_login_button.css', 'client');
   api.add_files("smartsheet.js");
